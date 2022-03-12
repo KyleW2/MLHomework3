ccfd6c9f91ef6df179d4c0b681aa7d417d0313f3
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-03-11 19:04:10 -0600

    Finished TD Lambda

--- Q4/TDLambda.py
+++ Q4/TDLambda.py
@@ -23,14 +23,25 @@ class TDLambda:
     def updateValue(self, state: str, reward: int, next_state: str) -> None:
         self.states[state].updateValue(self.alpha, self.gamma, reward, self.states[next_state].getValue())
     
+    def updateElig(self, state: str, is_state: bool):
+        self.states[state].updateElig(self.gamma, self.lam, is_state)
+    
     def runEpisode(self, episode: list) -> None:
         # Episodes must go: [[state -> action -> reward], ...]
         for i in range(0, len(episode)-1):
-            self.updateValue(episode[i][0], episode[i][2], episode[i+1][0])
-    
+            for k, v in self.states.items():
+                # If the state equals current at time step
+                if k == episode[i][0]:
+                    self.updateElig(episode[i][0], True)
+                    self.updateValue(episode[i][0], episode[i][2], episode[i+1][0])
+                else:
+                    self.updateValue(episode[i][0], episode[i][2], episode[i+1][0])
+                    self.updateElig(episode[i][0], False)
+            
     def printValues(self):
         for k, v in self.states.items():
             print("V(" + k + ") = " + str(v.getValue()))
+            print("E(" + k + ") = " + str(v.getElig()))
             
 
 
@@ -41,6 +52,8 @@ if __name__ == "__main__":
     ep1 = [["Olympus", "walk", 2], ["Dodoni", "fly", 2], ["Olympus", "fly", -1], ["Olympus"]]
     test.runEpisode(ep1)
     test.printValues()
+
+    print()
 
     ep2 = [["Olympus", "fly", 2], ["Delphi", "fly", 4], ["Delos"]]
     test.runEpisode(ep2)
